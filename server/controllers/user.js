5477d70f3e77c79b3b1b8492b16c9da20a977b68
lonewolf-0 <aryan.tyagi202@gmail.com>
2025-04-20 14:35:23 +0530

    first commit

---
+++ server/controllers/user.js
@@ -0,0 +1,43 @@
+import { User } from "../models/user.js";
+import bcrypt from "bcrypt";
+
+export const register = async (req, res) => {
+  try {
+    const { email, name, password } = req.body;
+
+    let user = await User.findOne({ email });
+
+    if (user)
+      return res.status(400).json({
+        message: "User already Exists",
+      });
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    user = {
+      name,
+      email,
+      password: hashPassword,
+    };
+
+    const otp = Math.floor(Math.random * 1000000);
+
+    const activationToken = jwt.sign(
+      {
+        user,
+        otp,
+      },
+      process.env.Activation_Secret,
+      {
+        expiresIn: "5m",
+      }
+    );
+    const data = {
+      name,
+      otp,
+    };
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
