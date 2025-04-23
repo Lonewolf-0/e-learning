300f212b26c1acbba395238df76a44d4001b22ec
lonewolf-0 <aryan.tyagi202@gmail.com>
2025-04-23 21:09:30 +0530

    checkout and pyment verification api

--- server/index.js
+++ server/index.js
@@ -1,6 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDb } from "./Database/db.js";
+import Razorpay from "razorpay";
+
+export const instance = new Razorpay({
+  key_id: process.env.Razorpay_Key,
+  key_secret: process.env.Razorpay_Secret,
+});
 
 dotenv.config();
 const app = express();
@@ -14,19 +20,15 @@ app.get("/", (req, res) => {
   res.send("server is working");
 });
 
-
 // importing routes
-import userRoutes from './routes/user.js';
-import courseRoutes from './routes/course.js';
-import adminRoutes from './routes/admin.js';
-
-
+import userRoutes from "./routes/user.js";
+import courseRoutes from "./routes/course.js";
+import adminRoutes from "./routes/admin.js";
 
 //using routes
 app.use("/api", userRoutes);
 app.use("/api", courseRoutes);
 app.use("/api", adminRoutes);
-
 
 app.listen(port, () => {
   console.log(`Server Running on http://localhost:${port}`);
--- server/routes/course.js
+++ server/routes/course.js
@@ -1,10 +1,12 @@
 import express from "express";
 import {
+  checkout,
   fetchLecture,
   fetchLectures,
   getAllCourses,
   getMyCourses,
   getSingleCourse,
+  paymentVerification,
 } from "../controllers/course.js";
 import { isAuth } from "../middlewares/isAuth.js";
 
@@ -15,5 +17,7 @@ router.get("/course/:id", getSingleCourse);
 router.get("/lectures/:id", isAuth, fetchLectures);
 router.get("/lecture/:id", isAuth, fetchLecture);
 router.get("/mycourse", isAuth, getMyCourses);
+router.post("/course/checkout/:id", isAuth, checkout);
+router.post("/verification/:id", isAuth, paymentVerification);
 
 export default router;
