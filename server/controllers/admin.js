9ac41f6e4aebaedd0fb868d6137395d8534a4415
lonewolf-0 <aryan.tyagi202@gmail.com>
2025-04-23 18:57:45 +0530

    delete lecture controller

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -2,6 +2,9 @@ import TryCatch from "../middlewares/TryCatch.js";
 import Courses from "../models/Courses.js";
 import { Lecture } from "../models/Lecture.js";
 import { rm } from "fs";
+import { promisify } from "util";
+import fs from "fs";
+import { User } from "../models/User.js";
 
 export const createCourse = TryCatch(async (req, res) => {
   const { title, description, category, createdBy, duration, price } = req.body;
@@ -54,3 +57,42 @@ export const deleteLecture = TryCatch(async (req, res) => {
 
   res.json({ message: "Lecture deleted" });
 });
+
+const unlinkAsync = promisify(fs.unlink);
+
+export const deleteCourse = TryCatch(async (req, res) => {
+  const course = await Courses.findById(req.params.id);
+
+  if (!course) {
+    return res.status(404).json({ message: "Course not found" });
+  }
+
+  const lectures = await Lecture.find({ course: course._id });
+
+  await Promise.all(
+    lectures.map(async (lecture) => {
+      try {
+        await unlinkAsync(lecture.video);
+        console.log("Video Deleted");
+      } catch (err) {
+        console.error("Error deleting video:", err);
+      }
+    })
+  );
+  rm(course.image, (err) => {
+    if (err) {
+      console.error("Error deleting thumbnail:", err);
+    } else {
+      console.log("thumbnail deleted");
+    }
+  });
+
+  await Lecture.deleteMany({ course: req.params.id });
+  await course.deleteOne();
+
+  await User.updateMany({}, { $pull: { subscription: req.params.id } });
+
+  res.json({ message: "Course Deleted" });
+});
+
+
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -3,6 +3,7 @@ import { isAdmin, isAuth } from "../middlewares/isAuth.js";
 import {
   addLecture,
   createCourse,
+  deleteCourse,
   deleteLecture,
 } from "../controllers/admin.js";
 import { uploadFiles } from "../middlewares/multer.js";
@@ -13,5 +14,7 @@ const router = express.Router();
 router.post("/course/new", isAuth, isAdmin, uploadFiles, createCourse);
 router.post("/course/:id", isAuth, isAdmin, uploadFiles, addLecture);
 router.delete("/lecture/:id", isAuth, isAdmin, deleteLecture);
+router.delete("/course/:id", isAuth, isAdmin, deleteCourse);
+
 
 export default router;
