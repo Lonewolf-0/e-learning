9e74837bf2f6a94095d7221d690c1cf8031376c7
lonewolf-0 <aryan.tyagi202@gmail.com>
2025-04-24 01:31:53 +0530

    feat: implement main application structure with routing and header component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import "./App.css";
 import{BrowserRouter, Route, Routes} from 'react-router-dom';
+import Home from "./pages/home/Home";
+import Header from "./components/header/Header";
 
 const App = () => {
-  return <div>app</div>;
+  return <>
+  <BrowserRouter>
+  <Header/>
+  <Routes>
+    <Route path="/" element={<Home/>}/>
+  </Routes>
+  </BrowserRouter>
+  
+  </>;
 };
 
 export default App;
