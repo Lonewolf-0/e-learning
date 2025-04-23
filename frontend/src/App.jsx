719b17a64cc8663798b24e53bb2beda880e0f85b
lonewolf-0 <aryan.tyagi202@gmail.com>
2025-04-24 00:59:43 +0530

    feat: initialize frontend project with React, Vite, and ESLint
    - Added package.json with dependencies for React, React Router, and Vite.
    - Included Vite configuration file for project setup.
    - Created main application entry point (main.jsx) and App component (App.jsx).
    - Added basic styling in App.css.
    - Implemented Header component with basic structure and styling.
    - Included SVG assets for Vite and React logos.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+import{BrowserRouter, Route, Routes} from 'react-router-dom';
+
+const App = () => {
+  return <div>app</div>;
+};
+
+export default App;
---
+++ frontend/src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./header.css";
+
+const Header = () => {
+  return (<header>
+    <div className="logo" >Elearning</div>
+    <div className="link"></div>
+    
+  </header>)
+};
+
+export default Header;
