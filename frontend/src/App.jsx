06bdc9ea884d23c2bdde2c52bea7b7d61e3eee72
lonewolf-0 <aryan.tyagi202@gmail.com>
2025-04-28 19:49:10 +0530

    feat: update React and ReactDOM to version 18.3.1 in frontend, add authentication pages and styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,26 @@
 import React from "react";
 import "./App.css";
-import{BrowserRouter, Route, Routes} from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Header from "./components/header/Header";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Verify from "./pages/auth/Verify";
 
 const App = () => {
-  return <>
-  <BrowserRouter>
-  <Header/>
-  <Routes>
-    <Route path="/" element={<Home/>}/>
-  </Routes>
-  </BrowserRouter>
-  
-  </>;
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/verify" element={<Verify />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default App;
