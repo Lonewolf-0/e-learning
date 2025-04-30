602174bd27366b30455e278613e2041eee54dbc5
lonewolf-0 <aryan.tyagi202@gmail.com>
2025-05-01 01:56:56 +0530

    feat: add Footer component, enhance Home page with navigation and testimonials

---
+++ frontend/src/components/footer/Footer.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./footer.css";
+import { AiFillFacebook } from "react-icons/ai";
+import { AiFillTwitterSquare } from "react-icons/ai";
+import { AiFillInstagram } from "react-icons/ai";
+
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+  return (
+    <footer>
+      <div className="footer-content">
+        <p>
+          &copy; {currentYear} Your Elearning Platform. All rights reserved.{" "}
+          <br />
+          Made with ❤️ <a href="https://github.com/lonewolf-0">Aryan Tyagi</a>
+        </p>
+        <div className="social-links">
+          <a href="">
+            <AiFillFacebook />
+          </a>
+          <a href="">
+            <AiFillTwitterSquare />
+          </a>
+          <a href="">
+            <AiFillInstagram />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
