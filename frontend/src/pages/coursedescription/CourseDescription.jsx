5fa92d74ca6db03a78dc37af4e01e2f9e66b9357
lonewolf-0 <aryan.tyagi202@gmail.com>
2025-05-04 01:25:58 +0530

    feat: implement payment processing and success page; enhance CourseDescription with checkout functionality and loading state

--- frontend/src/pages/coursedescription/CourseDescription.jsx
+++ frontend/src/pages/coursedescription/CourseDescription.jsx
@@ -1,43 +1,119 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./courseDescription.css";
 import { useNavigate, useParams } from "react-router-dom";
 import { CourseData } from "../../context/CourseContext";
 import { server } from "../../main";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { UserData } from "../../context/UserContext";
+import Loading from "../../components/loading/Loading";
 
 const CourseDescription = ({ user }) => {
   const params = useParams();
   const navigate = useNavigate();
-
-  const { fetchCourse, course } = CourseData();
+  const [loading, setLoading] = useState(false);
+  const { fetchUser } = UserData();
+  const { fetchCourse, course, fetchCourses } = CourseData();
 
   useEffect(() => {
     fetchCourse(params.id);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  const checkoutHandler = async () => {
+    const token = localStorage.getItem("token");
+    setLoading(true);
+    const {
+      data: { order },
+    } = await axios.post(
+      `${server}/api/course/checkout/${params.id}`,
+      {},
+      {
+        headers: {
+          token,
+        },
+      }
+    );
+    const options = {
+      key: "rzp_test_MxQzBVD7HffKYl", // Enter the Key ID generated from the Dashboard
+      amount: order.id, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+      currency: "INR",
+      name: "E Learning", //your business name
+      description: "Test Transaction",
+      order_id: order.id,
+
+      handler: async function (response) {
+        const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
+          response;
+
+        try {
+          const data = await axios.post(
+            `${server}/api/verification/${params.id}`,
+            {
+              razorpay_order_id,
+              razorpay_payment_id,
+              razorpay_signature,
+            },
+            {
+              headers: {
+                token,
+              },
+            }
+          );
+          await fetchUser();
+          await fetchCourses();
+          toast.success(data.message);
+          setLoading(false);
+          navigate(`/payment-success/${razorpay_payment_id}`);
+        } catch (error) {
+          toast.error(error.response.data.message);
+          setLoading(false);
+        }
+      },
+      theme: {
+        color: "#8a4baf",
+      },
+    };
+    const razorpay = new window.Razorpay(options);
+    razorpay.open();
+  };
 
   return (
     <>
-      {course && (
-        <div className="course-description">
-          <div className="course-header">
-            <img src={`${server}/${course.image}`} className="course-image" />
-            <div className="course-info">
-              <h2>{course.title}</h2>
-              <p>Instructor: {course.createdBy}</p>
-              <p>Duration: {course.duration} weeks</p>
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          {course && (
+            <div className="course-description">
+              <div className="course-header">
+                <img
+                  src={`${server}/${course.image}`}
+                  className="course-image"
+                />
+                <div className="course-info">
+                  <h2>{course.title}</h2>
+                  <p>Instructor: {course.createdBy}</p>
+                  <p>Duration: {course.duration} weeks</p>
+                </div>
+              </div>
+              <p>{course.description}</p>
+              <p>Let's get started with this course At ₹{course.price} </p>
+              {user && user.subscription.includes(course._id) ? (
+                <button
+                  onClick={() => navigate(`/course/study/${course._id}`)}
+                  className="common-btn"
+                >
+                  Study
+                </button>
+              ) : (
+                <button onClick={checkoutHandler} className="common-btn">
+                  Buy now
+                </button>
+              )}
             </div>
-          </div>
-          <p>Let's get started with this course At ₹{course.price} </p>
-          {user && user.subscription.includes(course._id) ? (
-            <button
-              onClick={() => navigate(`/course/study/${course._id}`)}
-              className="common-btn"
-            >
-              Study
-            </button>
-          ) : (
-            <button className="common-btn">Buy now</button>
           )}
-        </div>
+        </>
       )}
     </>
   );
